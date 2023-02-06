5c646d4d6a36a6a3299ae7ecab4c652db578a866
Marlowe Recht <recht.marlowe@gmail.com>
2023-02-06 17:35:17 +0100

    commit tree 15776b667632beb2ce40858cfe6ea1a69cea0dd9

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,17 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    song_array = songs.map { |song| song.genre == self}
+    song_array.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_array = self.artists.map { |artist| artist[:name] }
   end
 end
